fa0d83a97f1f7b21d693e83bf748024d1573df7d
Dimitar <dimitar.mandarliev@gmail.com>
2025-09-15 16:01:48 +0300

    chore: initialize project with package.json and Parcel setup

--- src/js/controller.js
+++ src/js/controller.js
@@ -12,3 +12,28 @@ const timeout = function (s) {
 // https://forkify-api.jonas.io
 
 ///////////////////////////////////////
+const showRecipe = async () => {
+  try {
+    const res = await fetch(
+      // "https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886"
+      "https://forkify-api.jonas.io/api/v2/recipes/664c8f193e7aa067e94e877e"
+    );
+    const data = await res.json();
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      published: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+  } catch (error) {
+    alert(error);
+  }
+};
+
+showRecipe();

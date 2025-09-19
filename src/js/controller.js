2f6b2d48fb3c04fbedc1e1cd45d860585aaed59b
Dimitar <dimitar.mandarliev@gmail.com>
2025-09-19 10:18:00 +0300

    feat: update recipe links and fetch logic in controller.js

--- src/js/controller.js
+++ src/js/controller.js
@@ -29,11 +29,15 @@ const renderSpinner = function (parentEl) {
 };
 const showRecipe = async () => {
   try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return;
     // 1) Loading recipe
     renderSpinner(recipeContainer);
     const res = await fetch(
       // "https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886"
-      "https://forkify-api.jonas.io/api/v2/recipes/664c8f193e7aa067e94e877e"
+      `https://forkify-api.jonas.io/api/v2/recipes/${id}`
     );
     const data = await res.json();
     let { recipe } = data.data;
@@ -154,4 +158,4 @@ const showRecipe = async () => {
   }
 };
 
-showRecipe();
+["hashchange", "load"].forEach((ev) => window.addEventListener(ev, showRecipe));
